80b4b1cbe1f255a121d4139a83d49a109ab350dc
Aless00san <99504826+Aless00san@users.noreply.github.com>
2022-02-11 19:48:19 +0100

    Add files via upload

---
+++ Actividad.js
@@ -0,0 +1,20 @@
+
+//con esta funcion añadimos el contenido del boton a la pantalla
+function myFunction(obj) {
+    var y = document.getElementById("pantalla");
+    var id = obj.id;
+    pantalla.innerHTML += id;
+  }
+//esta funcion limpia la pantalla (por si nos equivocamos)
+function limpiar() {
+  document.getElementById("pantalla").innerHTML = "";
+  }
+
+function calculate() {
+var contenido = document.getElementById("pantalla").innerHTML;
+document.getElementById("salida").innerHTML += contenido +"<br>";
+}
+
+function divisorCalculo(String pantalla){
+
+}
