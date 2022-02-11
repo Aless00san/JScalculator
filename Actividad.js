d453997db7710b5f2a117d3be5400c08ff1aa3f3
Aless00san <99504826+Aless00san@users.noreply.github.com>
2022-02-11 20:06:12 +0100

    añadido alerta suma

--- Actividad.js
+++ Actividad.js
@@ -10,13 +10,17 @@ function limpiar() {
   document.getElementById("pantalla").innerHTML = "";
   }
 
+//esta funcion se ejecuta cuando pulsamos igual, manda lo que tengamos en pantalla a la salida
 function calculate() {
 var contenido = document.getElementById("pantalla").innerHTML;
 document.getElementById("salida").innerHTML += contenido +"<br>";
+divisorCalculo(contenido);
 }
 
 function divisorCalculo(pantalla){
   for (var i = 0; i < pantalla.length; i++) {
+    if(pantalla.charAt(i) == '+'){
     alert(pantalla.charAt(i));
   }
+}
 }
